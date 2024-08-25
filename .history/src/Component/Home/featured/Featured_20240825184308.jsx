018e0f228dd639784f57789fc93eede497202233
Sahan Chathura <130049052+SahanChathura0730@users.noreply.github.com>
2024-08-25 22:59:24 +0530

    since to recent part

---
+++ .history/src/Component/Home/featured/Featured_20240825184308.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Featured.css'
+import FeaturedCard from './FeaturedCard'
+
+const Featured = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Featured
